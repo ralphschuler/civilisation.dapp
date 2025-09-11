c6e5c3dee909007696efb4966e436ca7441e6c19
Ralph Schuler <ralph.schuler@nyphon.de>
2025-09-12 01:01:47 +0200

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import MiniKitProvider from '@/providers/minikit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { MiniKitProvider, MiniKit } from '@worldcoin/minikit-js/minikit-provider'
 
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log(MiniKit.isInstalled())
+
   return (
     <MiniKitProvider>
       <QueryClientProvider client={queryClient}>
