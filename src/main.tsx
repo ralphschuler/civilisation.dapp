3777e60fcb54ffbae79f2d751b182f35aae5b106
Ralph Schuler <ralph.schuler@nyphon.de>
2025-09-12 11:16:38 +0200

    add react query & zustand

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Eruda } from './providers/eruda-provider.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Eruda>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Eruda>
   </StrictMode>,
 )
