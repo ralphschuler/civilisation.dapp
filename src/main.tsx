172efa0b5af8cd9d008a5b8d5e6b48119372286e
Ralph Schuler <ralph.schuler@nyphon.de>
2025-09-12 13:38:22 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,25 @@ import "@/global.css"
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Eruda } from '@/providers/eruda-provider';
 import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Eruda>
-      <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
-        <MiniKitProvider app-id={{appId: import.meta.env.VITE_PUBLIC_APP_ID}}>
+      <HashRouter
+        basename={import.meta.env.VITE_BASE_PATH}
+
+      >
+        <MiniKitProvider
+          app-id={{appId: import.meta.env.VITE_PUBLIC_APP_ID}}
+        >
           <QueryClientProvider client={queryClient}>
             <App />
           </QueryClientProvider>
         </MiniKitProvider>
-      </BrowserRouter>
+      </HashRouter>
     </Eruda>
   </StrictMode>,
 )
