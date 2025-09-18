079bac338c3c416f40e706a9a32c2893d717bfc4
Ralph Schuler <ralph.schuler@nyphon.de>
2025-09-18 19:48:19 +0200

    add linting

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,37 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    env: {
+      browser: true,
+      es2020: true,
+      node: true,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier.configs.recommended,
     ],
+    plugins: [prettier],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': ['warn'], // Prettier issues als ESLint-Warnungen
     },
   },
 ])
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
   root: '.',
   base: process.env.VITE_BASE_PATH,
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
