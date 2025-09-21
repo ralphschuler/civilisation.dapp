f3da8bd8e86d7eff0ced4377074282d48afb616f
Ralph Schuler <ralph.schuler@nyphon.de>
2025-09-21 10:08:40 +0200

    collect and upload abis as artifact

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,11 @@
 export default function Home() {
+  const { read, write } = useContract({
+    address: import.meta.env.VITE_PUBLIC_CONTRACT_ADDRESS
+    abi: Abi;
+    rpcUrl?: string;
+  });
+
+
   return (
     <>
       <div>HOME</div>
